faea75dd359d670cc0a53fabc7eb1401d4459eaa
Rocket8900 <74958008+Rocket8900@users.noreply.github.com>
2023-09-25 16:32:15 +0800

    feat: added driverlist page

--- afterzoom/src/App.js
+++ afterzoom/src/App.js
@@ -1,6 +1,7 @@
 import NavbarComponent from "./components/ui/Navbar";
 import { Home } from "./components/home/Home";
 import { Login } from "./components/login/Login";
+import DriversComponent from "./components/drivers/drivers";
 import {Routes,Route} from 'react-router';
 import React, { useState } from 'react';
 import './App.css';
@@ -12,6 +13,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/drivers" element={<DriversComponent />} />
+
       </Routes>
     </div>
   ) 
