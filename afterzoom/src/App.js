87f3442a116ea1be76bac8af711322f6eef905be
ZhiHui <kekzhihui@gmail.com>
2023-09-30 15:36:47 +0800

    Dashboards

--- afterzoom/src/App.js
+++ afterzoom/src/App.js
@@ -1,6 +1,8 @@
 import NavbarComponent from "./components/ui/Navbar";
 import { Home } from "./components/home/Home";
 import { Login } from "./components/login/Login";
+import { UserDashboard } from "./components/dashboards/UserDashboard";
+import { InstructorDashboard } from "./components/dashboards/InstructorDashboard";
 import {Routes,Route} from 'react-router';
 import React, { useState } from 'react';
 import './App.css';
@@ -12,7 +14,9 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        
+        <Route path="/user-dashboard" element={<UserDashboard />} />
+        <Route path="/instructor-dashboard" element={<InstructorDashboard />} />
+
       </Routes>
     </div>
   ) 
