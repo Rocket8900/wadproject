a90525d9730e6b7e742bd566ad33bce770c6acc0
seejaee <fooseejae@gmail.com>
2023-10-04 22:34:21 +0800

    feat: added json stuff

--- afterzoom/src/components/registration/registration.js
+++ afterzoom/src/components/registration/registration.js
@@ -1,59 +1,160 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Registration() {
-    return (
-        <div>
-            <div className="container-fluid">
-                <div class="row main">
+  const [formData, setFormData] = useState({
+    role: "",
+    fullName: "",
+    email: "",
+    password: "",
+    gender: "",
+    language: "",
+  });
 
-           
-            <div className="col-5 qnMan align-self-center">
-                <img src="questionMarkMan.jpg" alt=""/>
-            </div>
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
-  
-            <div className="col-7 align-self-center">
-                <h1>TELL ME MORE ABOUT YOURSELF</h1>
-                <div class="text-center m-3" style="padding-top: 10px;">
-                    <input type="checkbox" className="btn-check" id="btn-check" autocomplete="off"/>
-                    <label className="btn btn-dark btns" for="btn-check">LEARNER</label>
-                    <input type="checkbox" className="btn-check" id="btn-check" autocomplete="off"/>
-                    <label className="btn btn-dark btns" for="btn-check">INSTRUCTOR</label>
-                </div>
-                <div className="mb-3">
-                    <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Full Name"/>
-                </div>
-                <div className="mb-3">
-                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Email Address"/>
-                </div>
-                <div>
-                    <input type="password" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock" placeholder="Password"/>
-                </div>
-                <div id="passwordHelpBlock" class="form-text">
-                Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
-                </div>
-                <div className="text-center" style="padding-top: 10px;">
-                    <input type="checkbox" className="btn-check" id="btn-check" autocomplete="off"/>
-                    <label className="btn btn-info btns" for="btn-check">Male</label>
-                    <input type="checkbox" className="btn-check" id="btn-check" autocomplete="off"/>
-                    <label className="btn btn-danger btns" for="btn-check">Female</label>
-                </div>
-                <div style="padding-top: 10px;">
-                    <select className="form-select" aria-label="Default select example">
-                        <option selected disabled>Language</option>
-                        <option value="1">English</option>
-                        <option value="2">Chinese</option>
-                        <option value="3">Tamil</option>
-                        <option value="4">Malay</option>
-                  </select>
-                </div>
-                <div>
-                    <a href="" className="arrow pt-5"><img src="right-arrow-50.png" alt=""/></a>
-                </div>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("localhost:3001/v1/api/XXX/YYY", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        console.log("Form data submitted successfully!");
+      } else {
+        console.error("Error submitting form data");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <div>
+      <div className="container-fluid">
+        <div className="row main">
+          <div className="col-5 qnMan align-self-center">
+            <img src="questionMarkMan.jpg" alt="Question Mark Man" />
+          </div>
+
+          <div className="col-7 align-self-center">
+            <h1>TELL ME MORE ABOUT YOURSELF</h1>
+            <div className="text-center m-3" style={{ paddingTop: "10px" }}>
+              <label>
+                <input
+                  type="radio"
+                  name="role"
+                  value="learner"
+                  onChange={handleChange}
+                />{" "}
+                Learner
+              </label>{" "}
+              <label>
+                <input
+                  type="radio"
+                  name="role"
+                  value="instructor"
+                  onChange={handleChange}
+                />{" "}
+                Instructor
+              </label>
             </div>
+            <div className="mb-3">
+              <input
+                type="text"
+                name="fullName"
+                value={formData.fullName}
+                onChange={handleChange}
+                className="form-control"
+                placeholder="Full Name"
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="form-control"
+                placeholder="Email Address"
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                id="inputPassword5"
+                className="form-control"
+                aria-describedby="passwordHelpBlock"
+                placeholder="Password"
+              />
+            </div>
+            <div id="passwordHelpBlock" className="form-text">
+              Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.
+            </div>
+            <div className="text-center" style={{ paddingTop: "10px" }}>
+              <label>
+                <input
+                  type="radio"
+                  name="gender"
+                  value="male"
+                  onChange={handleChange}
+                />{" "}
+                Male
+              </label>{" "}
+              <label>
+                <input
+                  type="radio"
+                  name="gender"
+                  value="female"
+                  onChange={handleChange}
+                />{" "}
+                Female
+              </label>
+            </div>
+            <div style={{ paddingTop: "10px" }}>
+              <select
+                name="language"
+                value={formData.language}
+                onChange={handleChange}
+                className="form-select"
+                aria-label="Default select example"
+              >
+                <option value="" disabled>
+                  Language
+                </option>
+                <option value="english">English</option>
+                <option value="chinese">Chinese</option>
+                <option value="tamil">Tamil</option>
+                <option value="malay">Malay</option>
+              </select>
+            </div>
+            <div>
+              <a href="" className="arrow pt-5">
+                <img src="right-arrow-50.png" alt="Right Arrow" />
+              </a>
+            </div>
+            <button type="button" className="btn btn-primary" onClick={handleSubmit}>
+              Submit
+            </button>
+          </div>
         </div>
+      </div>
     </div>
-        </div>
-    )    
+  );
 }
-    export default Registration;
+
+export default Registration;
