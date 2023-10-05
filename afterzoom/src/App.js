52ea20873ef32cf9221339430c0265f0bdc72548
vynious <shawnthiah@gmail.com>
2023-10-05 10:49:46 +0800

    feat: jwt refresh/access token auth

--- afterzoom/src/App.js
+++ afterzoom/src/App.js
@@ -1,11 +1,11 @@
 import NavbarComponent from "./components/ui/Navbar";
 import { Home } from "./components/home/Home";
 import { Login } from "./components/login/Login";
-import { UserDashboard } from "./components/dashboards/UserDashboard";
+import { StudentDashboard } from "./components/dashboards/StudentDashboard";
 import { InstructorDashboard } from "./components/dashboards/InstructorDashboard";
 import  Registration  from "./components/registration/registration";
 import InstructorsComponent from "./components/instructors/instructors";
-import InstructorComponent from "./components/instructors/instructor";
+import InstructorDetail from "./components/instructors/InstructorDetail";
 import {Routes,Route} from 'react-router';
 import React from 'react';
 import './App.css';
@@ -21,10 +21,10 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
-        <Route path="/student-dashboard" element={<UserDashboard />} />
+        <Route path="/student-dashboard" element={<StudentDashboard />} />
         <Route path="/instructor-dashboard" element={<InstructorDashboard />} />
         <Route path="/instructors" element={<InstructorsComponent />} />
-        <Route path="/instructor" element={<InstructorComponent />} />
+        <Route path="/instructor" element={<InstructorDetail />} />
         <Route path="/chat" element={<Chat />} />
 
       </Routes>
