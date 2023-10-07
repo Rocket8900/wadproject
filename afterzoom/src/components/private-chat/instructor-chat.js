b758e39ee154f1262a402ba47a53e597d6777e32
vynious <shawnthiah@gmail.com>
2023-10-07 16:07:13 +0800

    feat: chat function

--- afterzoom/src/components/private-chat/instructor-chat.js
+++ afterzoom/src/components/private-chat/instructor-chat.js
@@ -4,35 +4,43 @@ import { io } from 'socket.io-client';
 import Cookies from 'js-cookie';
 import axios from 'axios';
 
-function ChatPage() {
+function ChatWithStudentPage() {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState('');
   const [socket, setSocket] = useState(null);
-  const [instructors, setInstructors] = useState([]);
-  const [selectedInstructorId, setSelectedInstructorId] = useState('');
+  const [students, setStudents] = useState([]);
+  const [selectedStudentId, setSelectedStudentId] = useState('');
 
   const token = Cookies.get('access_token');
 
   useEffect(() => {
-    const fetchInstructors = async () => {
+    const fetchStudents = async () => {
       try {
-        const response = await axios.get("http://localhost:3001/v1/api/instructor/list", {
+        const response = await axios.get("http://localhost:3001/v1/api/student/list", {
           headers: {
             Authorization: `Bearer ${token}`
           }
         });
-        setInstructors(response.data.data);
+        setStudents(response.data.data);
       } catch (error) {
-        console.error("Error fetching instructors:", error);
+        console.error("Error fetching students with chat history:", error);
       }
     };
 
-    fetchInstructors();
+    fetchStudents();
 
     const connectToSocketServer = () => {
       const newSocket = io('http://localhost:3001', {query: `token=${token}`});
-      newSocket.emit('connection', () => console.log('Connected to server'));
-      newSocket.on('private_message', (message) => setMessages(prevMessages => [...prevMessages, message]));
+
+      newSocket.emit('connection', () => {
+        console.log('Connected to server');
+      });
+
+      newSocket.on('private_message', (message) => {
+        console.log(message)
+        setMessages(prevMessages => [...prevMessages, message]);
+      });
+
       setSocket(newSocket);
     };
 
@@ -46,42 +54,48 @@ function ChatPage() {
     };
   }, []);
 
-  const handleInstructorChange = async (e) => {
-    setSelectedInstructorId(e.target.value);
-    setMessages([]); // Clear the current messages
-
+  const handleStudentChange = async (e) => {
+    setSelectedStudentId(e.target.value);
+    setMessages([])
+    console.log(e.target.value)
+    // Fetch the chat history with the selected student
     try {
       const response = await axios.get(`http://localhost:3001/v1/api/chat/${e.target.value}`, {
         headers: {
           Authorization: `Bearer ${token}`
         }
       });
+      console.log(response.data.data)
       if (response.data.data) {
-        setMessages(response.data.data.message);
+        setMessages(response.data.data.message); // Assuming the chat history is in the data property of the response=
       }
     } catch (error) {
       console.error("Error fetching chat history:", error);
     }
   };
+  
+
 
   const sendMessage = () => {
     if (input.trim()) {
       if (socket) {
-        socket.emit('private_message', {receiverId: selectedInstructorId, message: input});
+        socket.emit('private_message', {receiverId: selectedStudentId, message: input});
       }
       setInput('');
     }
   };
 
-  return (
+/* ... Your existing imports and component setup ... */
+
+return (
     <div className="chat-page">
       <div className="chat-header">
-        <h2>Chat with Instructor</h2>
-        <select value={selectedInstructorId} onChange={handleInstructorChange}>
-          <option value="">Select an instructor</option>
-          {instructors.map(instructor => (
-            <option key={instructor.id} value={instructor.id}>
-              {instructor.name}
+        <h2>Chat with Student</h2>
+        <select value={selectedStudentId} onChange={handleStudentChange}>
+          <option value="">Select a student</option>
+          {students.map(student => (
+            <option key={student.id} value={student.id}>
+              {student.name}
             </option>
           ))}
         </select>
@@ -95,11 +109,18 @@ function ChatPage() {
         ))}
       </div>
       <div className="chat-input">
-        <input value={input} onChange={(e) => setInput(e.target.value)} placeholder="Type a message..." />
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Type a message..."
+        />
         <button onClick={sendMessage}>Send</button>
       </div>
     </div>
   );
+  
+  /* ... Rest of your component ... */
+  
 }
 
-export default ChatPage;
+export default ChatWithStudentPage;
