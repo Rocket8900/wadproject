ee61ad00ab1e77204b1194fd7e673496ab4ac23d
clarissaksq <kohshiqi3@gmail.com>
2023-10-26 16:17:35 +0800

    feat : updated dashboard

--- afterzoom/src/components/dashboards/StudentDashboard.js
+++ afterzoom/src/components/dashboards/StudentDashboard.js
@@ -6,40 +6,21 @@ import MainContent from "./maincontent/MainContent";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import jwtDecode from "jwt-decode";
 
 export function StudentDashboard() {
     const [student, setStudent] = useState(null);
     const [bookings, setBookings] = useState(null);
     const { id } = useParams();
-    useEffect(() => {
-        const fetchStudentBookings = async () => {
-            try {
-                const token = getCookie("access_token");
-                const response = await axios.get(
-                    `http://localhost:3001/v1/api/booking/student/f111649e-a3b6-4d51-bd8b-5c5f7acc18a1`,
-                    {
-                        headers: {
-                            Authorization: `Bearer ${token}`,
-                        },
-                    }
-                );
-                setBookings(response.data.data);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-        fetchStudentBookings();
-    }, [id]);
-
-
-
 
     useEffect(() => {
         const fetchStudentProfile = async () => {
             try {
                 const token = getCookie("access_token");
+                const decodedToken = jwtDecode(token).user;
+                const studentId = decodedToken.id;
                 const response = await axios.get(
-                    `http://localhost:3001/v1/api/student/profile/f111649e-a3b6-4d51-bd8b-5c5f7acc18a1`,
+                    `http://localhost:3001/v1/api/student/profile/${studentId}`,
                     {
                         headers: {
                             Authorization: `Bearer ${token}`,
@@ -49,29 +30,44 @@ export function StudentDashboard() {
                 setStudent(response.data.data);
             } catch (error) {
                 console.error(error);
+            }
+        };
 
+        const fetchStudentBookings = async () => {
+            try {
+                const token = getCookie("access_token");
+                const decodedToken = jwtDecode(token).user;
+                const studentId = decodedToken.id;
+                const response = await axios.get(
+                    `http://localhost:3001/v1/api/booking/student/${studentId}`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    }
+                );
+                setBookings(response.data);
+            } catch (error) {
+                console.error(error);
             }
         };
 
         fetchStudentProfile();
-    }, [id]);
+        fetchStudentBookings();
+    }, []);
 
-
-    if (student === null) {
+    if (student === null || bookings === null) {
         return <div>Loading...</div>;
     }
-
-
 
     return (
         <Container fluid>
             <Row>
                 <Col lg={2} md={2} sm={2} id="sidebar">
-                    {/* Pass student data as a prop to Sidebar component */}
-                    <Sidebar student={student} />
+                    <Sidebar student={student} bookings={bookings} />
                 </Col>
                 <Col lg={10} md={10} sm={10} id="main-content">
-                    <MainContent student={student} />
+                    <MainContent student={student} bookings={bookings} />
                 </Col>
             </Row>
         </Container>
