f03b44e92d7bf09a95272fb5de6ac4d250dbf406
ZhiHui <kekzhihui@gmail.com>
2023-10-28 03:29:02 +0800

    feat: review quiz mistakes

--- afterzoom/src/App.js
+++ afterzoom/src/App.js
@@ -36,6 +36,8 @@ export default function App() {
         <Route path="/choose-quiz" element={<ChooseQuiz />} />
         <Route path="/btt-quiz" element={<Quiz type="btt"/>} />
         <Route path="/ftt-quiz" element={<Quiz type="ftt"/>} />
+        <Route path="/topical-quiz" element={<Quiz type="topic"/>} />
+        <Route path="/review-quiz" element={<Quiz type="review"/>} />
         
         <Route path="/post-feedback" element={<InstructorFeedbackPage />} />
         <Route path="/view-notebook" element={<StudentNotebook />} />
--- afterzoom/src/components/quiz/Quiz.js
+++ afterzoom/src/components/quiz/Quiz.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { btt_questions } from './btt'; // Import BTT questions
 import { ftt_questions } from './ftt'; // Import FTT questions
@@ -14,15 +15,38 @@ const Quiz = ({ type }) => {
     correctAnswers: 0,
     wrongAnswers: 0,
   })
+  const [mistakes, setMistakes] = useState([]);
 
-  
+
   let questions;
+
+  useEffect(() => {
+    // Check if the user has an existing mistakes array in the database
+    axios.get('http://localhost:3001/v1/api/quiz')
+      .then((response) => {
+        console.log(response.data)
+        console.log(response.data.data)
+
+        // If the user has a mistakes array, initialize "mistakes" accordingly
+        if (response.data.data !== null) {
+          // setUserHasMistakes(true);
+          setMistakes(response.data.data);
+          console.log("response.data is not null")
+          
+          // For "review," use the mistakes array as the question set
+          if (type === 'review') {
+            questions = mistakes;
+          }
+        }
+    });
+  }, [type]);
+  
 
   // Import the BTT and FTT questions accordingly
   if (type === 'btt') {
     questions = btt_questions
   }
-  else {
+  else if (type === 'ftt') {
     questions = ftt_questions
   };
   // console.log("bttquiz.js" + questions)
@@ -33,10 +57,15 @@ const Quiz = ({ type }) => {
         setSelectedAnswerIndex(null) // Reset selected answer index
         
       } else {
+        saveMistakesToDatabase();
         setShowResult(true);
       }
 
   }
+
+  useEffect(() => {
+    console.log('Mistakes:', mistakes);
+  }, [mistakes]);
 
   const onAnswerSelected = (index) => {
     
@@ -44,23 +73,62 @@ const Quiz = ({ type }) => {
         setSelectedAnswerIndex(index);
         const isCorrect = questions[activeQuestion].answers[index].correct;
   
-        if (isCorrect) {
+        if (!isCorrect) { // selected wrong answer
+          console.log(mistakes)
+
+          if (mistakes !== null) {
+
+            // Check if the question is already in the "mistakes" array
+            const found = mistakes.some((item) => {
+              return Object.keys(item).every((key) => questions[activeQuestion][key] === item[key]);
+            });
+            if (!found) { // question is not in mistakes array yet
+              // Update mistakes array
+              setMistakes([...mistakes, questions[activeQuestion]]);
+            }
+          } else {
+            // no existing mistakes
+            setMistakes([...mistakes, questions[activeQuestion]]);
+          }
+          // console.log(mistakes);
+
+          setResult((prev) => ({
+            ...prev,
+            wrongAnswers: prev.wrongAnswers + 1,
+          }));
+
+          
+        } else { // answer is correct
           setResult((prev) => ({
             ...prev,
             score: prev.score + 1,
             correctAnswers: prev.correctAnswers + 1,
           }));
-        } else {
-          setResult((prev) => ({
-            ...prev,
-            wrongAnswers: prev.wrongAnswers + 1,
-          }));
+
         }
       }
     
   }
 
   const addLeadingZero = (number) => (number > 9 ? number : `0${number}`)
+
+  const saveMistakesToDatabase = () => {
+    // Create a request object
+    const requestData = {
+      mistakes: mistakes,
+    };
+
+    // Send the "mistakes" array to the backend
+    axios.post('http://localhost:3001/v1/api/quiz', requestData)
+      .then((response) => {
+        // Handle the response if needed
+        console.log('Mistakes saved successfully:' + response.data);
+      })
+      .catch((error) => {
+        // Handle errors
+        console.error('Error saving mistakes:', error);
+      });
+  };
 
   const currentQuestion = questions[activeQuestion];
 
@@ -117,8 +185,12 @@ const Quiz = ({ type }) => {
           </p>
 
           <Link to="/choose-quiz">
-                <button id="btt" class="btn btn-outline-primary mb-4 text-center col-12 me-sm-4 col-sm-5">Return to quiz home</button>
-            </Link>
+            <button id="btt" class="btn btn-outline-primary mb-4 text-center col-12 me-sm-4 col-sm-5">Return to quiz home</button>
+          </Link>
+
+          <Link to="/review-quiz">
+            <button id="review" class="btn btn-outline-primary mb-4 text-center col-12 me-sm-4 col-sm-5">Review Mistakes</button>
+          </Link>
         </div>
       )}
     </div>
