c914c67ebadf5f60393ffdb829d9a8e67bd39e4c
clarissaksq <kohshiqi3@gmail.com>
2023-10-27 16:43:28 +0800

    feat anyhow push anyhow wack

---
+++ afterzoom/src/components/bookingLesson/bookingStudent.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import Sidebar from "../dashboards/sidebar/Sidebar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import jwtDecode from "jwt-decode";
+import BookingStudentContent from "./BookingStudentContent/BookingStudentContent";
+
+
+export function BookingStudent() {
+    const [student, setStudent] = useState(null);
+    const [bookings, setBookings] = useState(null);
+    const { id } = useParams();
+
+    useEffect(() => {
+        
+        const fetchData = async () => {
+            try {
+                const token = getCookie("access_token");
+                const decodedToken = jwtDecode(token).user;
+                const studentId = decodedToken.id;
+
+                const studentResponse = await axios.get(
+
+                    `http://localhost:3001/v1/api/student/profile/${studentId}`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    }
+                );
+                setStudent(studentResponse.data.data);
+
+
+                const bookingsResponse = await axios.get(
+                    `http://localhost:3001/v1/api/booking/student/`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    }
+                );
+                setBookings(bookingsResponse.data.data[0]);
+                
+            } catch (error) {
+                console.error(error);
+            }
+        };
+    
+        fetchData(); 
+        
+    }, []); 
+
+    if (bookings === null || student === null) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <Container fluid>
+            <Row>
+                <Col lg={2} md={2} sm={2} id="sidebar">
+                    <Sidebar student={student} bookings={bookings} />i
+                </Col>
+                <Col lg={10} md={10} sm={10} id="bookingStudentContent">
+                    <BookingStudentContent student={student} bookings={bookings} />
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(";").shift();
+}
