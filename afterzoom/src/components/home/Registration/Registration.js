8303e223a1bfd5741544c32e68b3ca453e1be6c5
Rocket8900 <74958008+Rocket8900@users.noreply.github.com>
2023-10-31 22:51:19 +0800

    feat: instructor profile view

--- afterzoom/src/components/home/Registration/Registration.js
+++ afterzoom/src/components/home/Registration/Registration.js
@@ -5,6 +5,9 @@ import MyForm from './components/Form';
 import { motion as m } from 'framer-motion'
 import { useState } from "react";
 import { Button, Modal } from "react-bootstrap";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const RegiModal = ({ isRegiOpen, setRegiOpen }) => {
   const { step, next, prev, goto } = useHandleForm(['0', '1', '2', '3', '4']);
@@ -14,14 +17,24 @@ const RegiModal = ({ isRegiOpen, setRegiOpen }) => {
       <div
         style={{ backdropFilter: "blur(5px)", display: "flex", alignItems: "center", justifyContent: "center"}}
       >
+
         <m.main
           initial={{ y: '-100%' }}
           animate={{ y: '0%' }}
           className='form-box'
         >
-          <Info step={step} />
-          <MyForm step={step} next={next} prev={prev} goto={goto} />
+          <Container>
+            <Row>
+              <Col lg={4} md={12}>
+                <Info step={step} />
+              </Col>
+              <Col lg ={8} md={12}>
+                <MyForm step={step} next={next} prev={prev} goto={goto} />
+              </Col>
+            </Row>
+          </Container>
         </m.main>
+
       </div>
     </Modal>
   );
