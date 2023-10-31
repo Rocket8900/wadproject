b0e68d8c70607a970fa17830727d1a134712e291
Rocket8900 <74958008+Rocket8900@users.noreply.github.com>
2023-10-31 20:17:17 +0800

    feat: instructors able to update booking

--- afterzoom/src/components/instructors/InstructorLessonList.js
+++ afterzoom/src/components/instructors/InstructorLessonList.js
@@ -7,6 +7,8 @@ import InstructorSidebar from '../dashboards/sidebar/instructorSidebar'
 import Cookie from 'js-cookie';
 import jwtDecode from "jwt-decode";
 import { useParams } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
+import Button from 'react-bootstrap/Button';
 
 function getCookie(name) {
   const value = `; ${document.cookie}`;
@@ -16,10 +18,12 @@ function getCookie(name) {
 
 function formatDateToReadableString(dateString) {
   const date = new Date(dateString);
-  return date.toLocaleString(); // You can customize the format as needed
+  return date.toLocaleString(); 
 }
 
 function InstructorLessonList() {
+  const [selectedLessonFeedback, setSelectedLessonFeedback] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
   const [instructor, setInstructor] = useState(null);
   const [bookings, setBookings] = useState([]);
   const [studentNames, setStudentNames] = useState({});
@@ -38,7 +42,7 @@ function InstructorLessonList() {
             headers: {
               Authorization: `Bearer ${token}`,
             },
-          }
+          } 
         );
         setInstructor(instructorResponse.data.data);
       } catch (error) {
@@ -71,6 +75,83 @@ function InstructorLessonList() {
     fetchBookings();
   }, []);
 
+  const FeedBackModal = ({ isOpen, setIsOpen, booking }) => {
+    const [feedback, setFeedback] = useState(selectedLessonFeedback);
+    const handleFeedbackChange = (e) => {
+      setFeedback(e.target.value);  
+    };
+
+    const handleSaveChanges = async () => {
+      try {
+        const token = getCookie("access_token");
+    
+        const response = await axios.patch(
+          `http://localhost:3001/v1/api/lesson/${booking.id}`, 
+          { feedback },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        console.log(booking)
+        console.log(booking.id)
+        // Check if the request was successful
+        if (response.status === 201) {
+          window.location.reload();
+          setIsOpen(false);
+    
+          // You can optionally update the UI or show a success message here
+        } else {
+          console.log(response)
+          // Handle the case where the request was not successful
+          console.error("Failed to update feedback");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    return (
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            onClick={() => setIsOpen(false)}
+            className="bg-slate-900/20 backdrop-blur p-8 fixed inset-0 z-50 grid place-items-center overflow-y-scroll cursor-pointer"
+          >
+            <motion.div
+              initial={{ scale: 0, rotate: "12.5deg" }}
+              animate={{ scale: 1, rotate: "0deg" }}
+              exit={{ scale: 0, rotate: "0deg" }}
+              onClick={(e) => e.stopPropagation()}
+            >
+              <div>
+                <h3>Edit the feedback for this lesson</h3>
+                <textarea
+                  type="textarea"
+                  value={feedback}
+                  onChange={handleFeedbackChange}
+                  className="w-full p-2 mb-6 rounded border"
+                textarea/>
+                <div className="flex gap-2">
+                  <button onClick={() => setIsOpen(false)} className="py-2 rounded">
+                    Nah, go back
+                  </button>
+                  <button onClick={handleSaveChanges} className="py-2 rounded">
+                    Save Changes
+                  </button>
+                </div>
+              </div>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    );
+  };
+
   const getStudentName = async (studentId) => {
     try {
       const token = getCookie("access_token");
@@ -85,7 +166,7 @@ function InstructorLessonList() {
       return response.data.data.name;
     } catch (error) {
       console.error(error);
-      return 'N/A'; // Return a default value in case of an error
+      return 'N/A'; 
     }
   };
 
@@ -105,7 +186,6 @@ function InstructorLessonList() {
     return <div>Loading...</div>;
   }
 
-  // Separate upcoming and past lessons
   const currentDate = new Date();
   const upcomingLessons = [];
   const pastLessons = [];
@@ -139,6 +219,7 @@ function InstructorLessonList() {
                 <th>Lesson Date</th>
                 <th>Lesson Description</th>
                 <th>Lesson Feedback</th>
+                <th>Modify Feedback</th>
               </tr>
             </thead>
             <tbody>
@@ -149,6 +230,7 @@ function InstructorLessonList() {
                   <td>{formatDateToReadableString(lesson.date)}</td>
                   <td>{lesson.description}</td>
                   <td>{lesson.feedback}</td>
+                  <td></td>
                 </tr>
               ))}
             </tbody>
@@ -163,6 +245,7 @@ function InstructorLessonList() {
                 <th>Lesson Date</th>
                 <th>Lesson Description</th>
                 <th>Lesson Feedback</th>
+                <th>Edit Feedback</th>
               </tr>
             </thead>
             <tbody>
@@ -173,6 +256,18 @@ function InstructorLessonList() {
                   <td>{formatDateToReadableString(lesson.date)}</td>
                   <td>{lesson.description}</td>
                   <td>{lesson.feedback}</td>
+                  <td>
+                    <Button
+                      variant="primary"
+                      onClick={() => {
+                        setSelectedLessonFeedback(lesson.feedback);
+                        setIsOpen(true);
+                      }}
+                    >
+                      Edit Feedback
+                    </Button>{' '}
+                  </td>
+                  <FeedBackModal isOpen={isOpen} setIsOpen={setIsOpen} booking={lesson} />
                 </tr>
               ))}
             </tbody>
