70674bbb56fdf7ef0fdcb39a88a2c70848d54650
vynious <shawnthiah@gmail.com>
2023-11-01 21:00:34 +0800

    bug instructor s3 img retrieval

--- backend/server/domains/s3/s3Route.js
+++ backend/server/domains/s3/s3Route.js
@@ -3,14 +3,21 @@
 
 import express from "express"
 import S3Controller from "./s3Controller.js"
+import AuthController from "../auth/authController.js"
+
 
 
 
 const s3Route = express.Router()
 
 
-s3Route.get("/bulk/:id", S3Controller.bulkRetrieveSignedUrlsBasedOnInstructorId)
-s3Route.get("/single/:id", S3Controller.singleRetrieveSignedUrlsBasedOnStudentId)
+s3Route.use(AuthController.validateUser)
+
+s3Route.get("/instructor/bulk", S3Controller.bulkRetrieveSignedUrlsBasedOnInstructorId)
+s3Route.get("/instructor/single", S3Controller.singleRetrieveSignedUrlsBasedOnInstructorId)
+
+s3Route.get("/student/single", S3Controller.singleRetrieveSignedUrlsBasedOnStudentId)
+s3Route.get("/instructor/single", S3Controller.singleRetrieveSignedUrlsBasedOnInstructorId)
 
 
 export {s3Route as default}
