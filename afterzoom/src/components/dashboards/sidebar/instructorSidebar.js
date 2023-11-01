f1f68e1620d66fc0f24d2dc6fac033ff3de46d73
Rocket8900 <74958008+Rocket8900@users.noreply.github.com>
2023-11-01 17:22:19 +0800

    feat: edit profile for instructor

--- afterzoom/src/components/dashboards/sidebar/instructorSidebar.js
+++ afterzoom/src/components/dashboards/sidebar/instructorSidebar.js
@@ -16,36 +16,77 @@ import {
 } from "react-icons/fa";
 import "react-pro-sidebar/dist/css/styles.css";
 import "./Sidebar.css";
-import { Link, Routes, Route, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Cookies from 'js-cookie';
-import profile from './sampleprofile.jpg'
+import profile from './sampleprofile.jpg';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import axios from 'axios'; // Import Axios
 
-
-function ProfileModal(props) {
-  const { instructor } = props;
+const ProfileModal = ({ modalProps, instructor }) => {
   const [name, setName] = useState(instructor.name);
   const [carModel, setCarModel] = useState(instructor.carModel);
   const [imageFile, setImageFile] = useState(null);
+
   const handleProfileUpdate = (e) => {
-    console.log('Updated Name:', name);
-    console.log('Updated Car Model:', carModel);
-    if (imageFile) {
-      console.log('Selected Image File:', imageFile);
+    // Update name and car model
+    const data = {
+      name: name,
+      carModel: carModel,
+    };
+
+    function getCookie(name) {
+      const value = `; ${document.cookie}`;
+      const parts = value.split(`; ${name}=`);
+      if (parts.length === 2) return parts.pop().split(";").shift();
     }
+
+    axios.patch('http://localhost:3001/v1/api/instructor/profile/', data)
+      .then((response) => {
+        console.log('Name and car model updated successfully');
+      })
+      .catch((error) => {
+        console.error('Error updating name and car model:', error);
+      });
+
+    // Update image
+    if (imageFile) {
+      const formData = new FormData();
+      formData.append('photo', imageFile);
+      const token = getCookie("access_token");
+      axios.patch('http://localhost:3001/v1/api/instructor/profile/photo', formData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((response) => {
+          console.log('Image updated successfully');
+        })
+        .catch((error) => {
+          console.error('Error updating image:', error);
+        });
+    }
+  }
+
+  const handleInstructorName = (e) => {
+    setName(e);
+  }
+
+  const handleCarModel = (e) => {
+    setCarModel(e);
   }
 
   const handleImageUpload = (e) => {
     const selectedFile = e.target.files[0];
     setImageFile(selectedFile);
   };
+
   return (
     <Modal
-      {...props}
+      {...modalProps}
       size="lg"
       aria-labelledby="contained-modal-title-vcenter"
       centered
@@ -55,25 +96,32 @@ function ProfileModal(props) {
           Edit profile
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body>  
+      <Modal.Body>
         <div>
           <label>Upload Image:</label>
-          <input type="file" onChange={handleImageUpload}  />
+          <input type="file" onChange={handleImageUpload} />
         </div>
         <div>
           <label>Name:</label>
-          <input type="text" value={instructor.name}  onChange={(e) => setName(e.target.value)} />
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => handleInstructorName(e.target.value)}
+          />
         </div>
         <div>
           <label>Car:</label>
-          <input type="text" value={instructor.carModel} onChange={(e) => setCarModel(e.target.value)}
- />
+          <input
+            type="text"
+            value={carModel}
+            onChange={(e) => handleCarModel(e.target.value)}
+          />
         </div>
       </Modal.Body>
 
       <Modal.Footer>
         <Button onClick={handleProfileUpdate}>Submit</Button>
-        <Button onClick={props.onHide}>Close</Button>
+        <Button onClick={modalProps.onHide}>Close</Button>
       </Modal.Footer>
     </Modal>
   );
@@ -81,7 +129,8 @@ function ProfileModal(props) {
 
 const Sidebar = ({ instructor }) => {
   const [isSmallScreen, setIsSmallScreen] = useState(false);
-  const [modalShow, setModalShow] = React.useState(false);
+  const [modalShow, setModalShow] = useState(false);
+
   useEffect(() => {
     const handleResize = () => {
       setIsSmallScreen(window.innerWidth <= 9);
@@ -98,14 +147,13 @@ const Sidebar = ({ instructor }) => {
 
   const { name, instructorId } = instructor;
   const [activeMenuItem, setActiveMenuItem] = useState("dashboard");
+
   const handleMenuItemClick = (menuItem) => {
     setActiveMenuItem(menuItem);
-  };
-  const handleDeleteCookie = () => {
-    Cookies.remove('access_token');
-    <Link to={'/'}></Link>
   }
+
   const navigate = useNavigate();
+
   const navigateToHome = () => {
     navigate('/');
   };
@@ -113,15 +161,17 @@ const Sidebar = ({ instructor }) => {
   if (name === null || instructorId === null) {
     return <div>Loading...</div>;
   }
-  console.log(instructor)
+
   return (
     <div id="sidebar">
-                    <ProfileModal
-              show={modalShow}
-              onHide={() => setModalShow(false)}
-              instructor={instructor}
-            />
-            
+      <ProfileModal
+        modalProps={{
+          show: modalShow,
+          onHide: () => setModalShow(false),
+        }}
+        instructor={instructor}
+      />
+
       <ProSidebar>
         <SidebarHeader>
           <div className="logotext">
@@ -129,24 +179,18 @@ const Sidebar = ({ instructor }) => {
           </div>
         </SidebarHeader>
 
-
         <SidebarContent>
-        {/* {!isSmallScreen && ( */}
           <div className="user-info-box" onClick={() => setModalShow(true)}>
             <Container>
               <Row>
-                <Col lg = {5} md={12}>
-                    <img className="sidebarprofile" src={profile} alt="Logo" />
-                  </Col>
-                <Col lg = {7} md={12}><p>Hi {name}!</p></Col>
+                <Col lg={5} md={12}>
+                  <img className="sidebarprofile" src={profile} alt="Logo" />
+                </Col>
+                <Col lg={7} md={12}><p>Hi {name}!</p></Col>
               </Row>
             </Container>
-            <div className="user-info">
-
-
-            </div>
+            <div className="user-info"></div>
           </div>
-        {/* )} */}
 
           <Menu iconShape="square">
             <MenuItem
@@ -154,7 +198,7 @@ const Sidebar = ({ instructor }) => {
               active={activeMenuItem === "dashboard"}
               onClick={() => handleMenuItemClick("dashboard")}
             >
-              <Link to="/instructor-dashboard" ><div className="mostInner">Dashboard</div></Link>
+              <Link to="/instructor-dashboard"><div className="mostInner">Dashboard</div></Link>
             </MenuItem>
 
             <MenuItem
@@ -186,7 +230,6 @@ const Sidebar = ({ instructor }) => {
                 <div className="mostInner">Open your chat</div>
               </Link>
             </MenuItem>
-
           </Menu>
         </SidebarContent>
 
