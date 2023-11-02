993637921a49987627b05d3d55fa7f0920e3c686
clarissaksq <kohshiqi3@gmail.com>
2023-11-03 04:49:13 +0800

    feat still hv flu im dying

--- afterzoom/src/components/instructorsList/MapView.js
+++ afterzoom/src/components/instructorsList/MapView.js
@@ -1,10 +1,17 @@
-import React, { useEffect } from "react";
 
-const MapView = (instructors) => {
+
+
+
+import React, { useEffect, useRef } from "react";
+
+const MapView = ({markerCoordinates}) => {
+  const mapRef = useRef(null);
+  const searchInputRef = useRef(null);
+
   useEffect(() => {
     // Initialize the map when the component mounts
     const script = document.createElement("script");
-    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyC2Qnl98e6FirAZSVRYEyzYfs_0jPaTsSk&callback=initMap&v=weekly`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyC2Qnl98e6FirAZSVRYEyzYfs_0jPaTsSk&libraries=places&callback=initMap&v=weekly`;
     script.defer = true;
     script.async = true;
 
@@ -18,34 +25,60 @@ const MapView = (instructors) => {
 
   // Define the initMap function here
   window.initMap = () => {
-    const map = new window.google.maps.Map(document.getElementById("map"), {
+    const map = new window.google.maps.Map(mapRef.current, {
       zoom: 10,
       center: { lat: -33.9, lng: 151.2 },
     });
 
-    // Data for the markers
-    const beaches = [
-      ["Bondi Beach", -33.890542, 151.274856, 4],
-      ["Coogee Beach", -33.923036, 151.259052, 5],
-      ["Cronulla Beach", -34.028249, 151.157507, 3],
-      ["Manly Beach", -33.80010128657071, 151.28747820854187, 2],
-      ["Maroubra Beach", -33.950198, 151.259302, 1],
-    ];
+    const autocomplete = new window.google.maps.places.Autocomplete(searchInputRef.current);
 
-    // Add markers to the map
-    for (let i = 0; i < beaches.length; i++) {
-      const beach = beaches[i];
+    autocomplete.addListener("place_changed", () => {
+      const place = autocomplete.getPlace();
+      if (!place.geometry) {
+        // User entered the name of a Place that was not suggested and
+        // pressed the Enter key, or the Place Details request failed.
+        return;
+      }
 
+      // Center the map on the selected place
+      map.setCenter(place.geometry.location);
+      map.setZoom(14); // You can adjust the zoom level as per your preference
+
+      // Add a marker for the selected place
       new window.google.maps.Marker({
-        position: { lat: beach[1], lng: beach[2] },
+        position: place.geometry.location,
         map: map,
-        title: beach[0],
-        zIndex: beach[3],
+        title: place.name,
       });
-    }
+    });
+
+    markerCoordinates.forEach(({ name, latitude, longitude }) => {
+        addCustomMarker(latitude, longitude, name);
+      });
+
+      const addCustomMarker = (latitude, longitude, title) => {
+        new window.google.maps.Marker({
+          position: { lat: latitude, lng: longitude },
+          map: mapRef.current,
+          title: title,
+        });
+      };
   };
 
-  return <div id="map" style={{ height: "500px", width: "100%" }}></div>;
+
+  return (
+    <div>
+      <div style={{ marginBottom: "10px" }}>
+        <input
+          ref={searchInputRef}
+          type="text"
+          placeholder="Enter an address"
+          style={{ width: "300px", padding: "10px" }}
+        />
+      </div>
+      <div id="map" ref={mapRef} style={{ height: "500px", width: "100%" }}></div>
+    </div>
+  );
 };
 
 export default MapView;
--- afterzoom/src/components/instructorsList/instructors.js
+++ afterzoom/src/components/instructorsList/instructors.js
@@ -21,6 +21,29 @@ function getCookie(name) {
   if (parts.length === 2) return parts.pop().split(";").shift();
 }
 
+
+const reverseGeocode = async (latitude, longitude) => {
+  try {
+    const response = await axios.get(
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyC2Qnl98e6FirAZSVRYEyzYfs_0jPaTsSk`
+    );
+
+    const results = response.data.results;
+    if (results && results.length > 0) {
+      const formattedAddress = results[0].formatted_address;
+      console.log("Formatted Address:", formattedAddress);
+      return formattedAddress;
+    } else {
+      console.log("No address found for the given coordinates.");
+      return null;
+    }
+  } catch (error) {
+    console.error("Error reverse geocoding:", error);
+    return null;
+  }
+};
+
+
 // function for setting background image
 const backgroundImageStyle = (urls) => {
   console.log(urls)
@@ -34,8 +57,19 @@ const backgroundImageStyle = (urls) => {
   }
 };
 
+
+
+
 // component for creating Instructor Card
-function InstructorCard({ instructor, showModal }) {
+function InstructorCard({ instructor, showModal, onAddMarker }) {
+
+  const handleAddMarkerClick = () => {
+    const { name, preferedLocation } = instructor;
+    if (preferedLocation && preferedLocation.latitude && preferedLocation.longitude) {
+      onAddMarker(name, preferedLocation.latitude, preferedLocation.longitude);
+    }
+  };
+
   const [modalShow, setModalShow] = useState(false);
 
   const handleModalShow = () => {
@@ -87,7 +121,11 @@ function InstructorCard({ instructor, showModal }) {
     <p>Gender: {instructor.gender}</p>
     <p>Affiliation: {instructor.affiliation}</p>
     <p>Transmission: {instructor.type}</p>
+    {/* <p>Preferred Location: {reverseGeocode(instructor.preferedLocation.latitude, instructor.preferedLocation.longitude)};</p> */}
     <Link to={`/student-chat`}><Button variant="dark">Chat with instructor!</Button></Link>
+    {instructor.preferedLocation && (
+        <button onClick={handleAddMarkerClick}>Add Marker</button>
+      )}
   </div>
 </Modal.Body>
           <Modal.Footer>
@@ -106,6 +144,7 @@ function InstructorCard({ instructor, showModal }) {
 
 
 function InstructorsComponent() {
+  const [markerCoordinates, setMarkerCoordinates] = useState([]);
   const [instructors, setInstructors] = useState([]);
   const [student, setStudent] = useState(null);
   const [bookings, setBookings] = useState(null);
@@ -115,6 +154,10 @@ function InstructorsComponent() {
     affiliation: [],
     type: []
   });
+
+  const handleAddMarker = (name, latitude, longitude) => {
+    setMarkerCoordinates([...markerCoordinates, { name, latitude, longitude }]);
+  };
 
   useEffect(() => {
     const getInstructorsData = async () => {
@@ -223,7 +266,7 @@ if (bookings === null || student === null) {
                         <div className={styles.FilterBox}>
 
                         <fieldset>
-                          <MapView/>
+                          <MapView markerCoordinates={markerCoordinates} />
                         </fieldset>
 
                         <br/>
@@ -338,7 +381,7 @@ if (bookings === null || student === null) {
                         <Row>
                           {filteredInstructors.map((instructor) => (
                             <Col key={instructor.id} lg={4} md={6} sm={12}>
-                              <InstructorCard instructor={instructor} />
+                              <InstructorCard instructor={instructor} onAddMarker={handleAddMarker}  />
                             </Col>
                           ))}
                         </Row>
