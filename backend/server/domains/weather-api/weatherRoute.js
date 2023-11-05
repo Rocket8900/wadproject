345b2ec8a507eeb8902e0592c08b323b54f09d8d
vynious <shawnthiah@gmail.com>
2023-11-05 14:44:00 +0800

    external weather api

---
+++ backend/server/domains/weather-api/weatherRoute.js
@@ -0,0 +1,21 @@
+import express from "express"
+import AuthController from "../auth/authController.js"
+import axios from "axios";
+import Logging from "../../utils/loggings.js";
+
+export const weatherRoute = express.Router();
+
+weatherRoute.use(AuthController.validateUser)
+weatherRoute.post("/", async (req, res) => {
+    try {
+        const {lat, lon, dt, apiKey} = req.body
+        const result = await axios.get(`https://api.openweathermap.org/data/3.0/onecall/timemachine?lat=${lat}&lon=${lon}&dt=${dt}&appid=${apiKey}`)
+        Logging.info('get weather details')
+        res.status(200).json({data: result.data})
+    } catch (error) {
+        Logging.error(error.message)
+        res.status(500).json({data:"internal server error"});
+    }
+})
+
+export {weatherRoute as default}
